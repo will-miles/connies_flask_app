2acb7da3948062436a763168136d19fe03eb3b48
will-miles <willmiles@live.co.uk>
2025-04-08 14:57:01 -0600

    tidying up

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request
-import aws_controller
 import utils
 
 app = Flask(__name__)
@@ -8,34 +7,17 @@ app = Flask(__name__)
 def index():
     return "This is the main page."
 
+# 127.0.0.1:5000/get-crags?style=winter&radius=50&lat=53.52574940018496&long=-1.919685834247175
 @app.route('/get-crags')
 def get_crags():
     lat = request.args.get('lat')
     lon = request.args.get('long')
     radius = request.args.get('radius')
     style = request.args.get('style')
-    print(style)
-    allCrags = aws_controller.get_crags(style)
-    deserializedData = utils.deserialize(allCrags)
 
-    matchingCrags = deserializedData['Items']
-
-    # 127.0.0.1:5000/get-crags?style=winter&radius=50&lat=53.52574940018496&long=-1.919685834247175
-
-    def check_distance(crag):
-        distance = utils.getDistanceFromLatLonInKm(float(lat), float(lon), float(crag['lat']), float(crag['long']))
-        print(distance, int(radius))
-
-        if distance < int(radius):
-            crag['distance'] = int(distance)
-            return True
-        return False
-
-    valid_crags_iterator = filter(check_distance, matchingCrags)
-
-    valid_crags = list(valid_crags_iterator)
-
-    return jsonify(valid_crags)
+    crags = utils.getAndFilterCrags(lat, lon, radius, style)
+    cragsWithWeather = uitls.addWeatherToCrags()
+    return jsonify(crags)
 
 if __name__ == '__main__':
     app.run()
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import math
 from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
+import aws_controller
 
 deserializer = TypeDeserializer()
 serializer = TypeSerializer()
 
+# converts dynamodb object to list/dict
 def deserialize(data):
     if isinstance(data, list):
         return [deserialize(v) for v in data]
@@ -16,6 +18,7 @@ def deserialize(data):
     else:
         return data
 
+# converts list/dict to dynamodb object
 def serialize(data):
     if isinstance(data, list):
         return [serialize(v) for v in data]
@@ -28,11 +31,34 @@ def serialize(data):
     else:
         return data
 
+# calculates distance between 2 lat,long coordinates
 def getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2):
   R = 6371 # Radius of the earth in km
-  dLat = math.radians(lat2 - lat1) # math.radians below
+  dLat = math.radians(lat2 - lat1)
   dLon = math.radians(lon2 - lon1)
   a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dLon / 2) * math.sin(dLon / 2)
   c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
   d = R * c # Distance in km
   return d
+
+# returns list of crags with matching style and locations within radius
+def getAndFilterCrags(lat, lon, radius, style):
+    # get all crags of matching climbing_style
+    allCrags = aws_controller.get_crags(style)
+    # deserialize response
+    deserializedData = deserialize(allCrags)
+
+    matchingCrags = deserializedData['Items']
+
+    def check_distance(crag):
+        distance = getDistanceFromLatLonInKm(float(lat), float(lon), float(crag['lat']), float(crag['long']))
+
+        if distance < int(radius):
+            crag['distance'] = int(distance)
+            return True
+        return False
+
+    valid_crags_iterator = filter(check_distance, matchingCrags)
+
+    return valid_crags = list(valid_crags_iterator)
+
