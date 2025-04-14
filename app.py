d9ca94c0fb5bcab686dc2f2eecf5eec6c686e200
will-miles <willmiles@live.co.uk>
2025-04-14 14:35:14 -0600

    cors headers set for local host

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
 import utils
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/get-crags": {"origins": "localhost:3000"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def index():
@@ -9,9 +12,10 @@ def index():
 
 # 127.0.0.1:5000/get-crags?style=winter&radius=50&lat=53.52574940018496&long=-1.919685834247175
 @app.route('/get-crags')
+@cross_origin(origin='localhost:3000',headers=['Content-Type','Authorization'])
 def get_crags():
     lat = request.args.get('lat')
-    lon = request.args.get('long')
+    lon = request.args.get('lon')
     radius = request.args.get('radius')
     style = request.args.get('style')
 
--- aws_controller.py
+++ aws_controller.py
@@ -4,9 +4,9 @@ dynamo_client = boto3.client('dynamodb', region_name='us-east-1')
 
 def get_crags(style):
     return dynamo_client.query(
-    TableName='crags',
-    KeyConditionExpression='climbing_style = :climbing_style',
-    ExpressionAttributeValues={":climbing_style":{"S": style}},
+        TableName='crags',
+        KeyConditionExpression='climbing_style = :climbing_style',
+        ExpressionAttributeValues={":climbing_style":{"S": style}},
     )
 
 def put_crag(crag):
